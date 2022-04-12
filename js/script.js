aa7484901db63dca732b7ef341d10352e80e472c
Glitch (indecisive-equable-scale) <none>
2022-04-12 01:08:20 +0000

    🚘🧪 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
-var userInput = $(".search-term").val();
-
-var api_request = `https://api.giphy.com/v1/gifs/search?q=${userInput}&rating=pg&api_key=dc6zaTOxFJmzC`;
-
-var randomNumber = Math.floor(Math.random() * 51);
-
 $(".search-button").click(function () {
+  var userInput = $(".search-term").val();
+  
+  var api_request = `https://api.giphy.com/v1/gifs/search?q=${userInput}&rating=pg&api_key=kVDaGVdpWz9cRhh2NgHxVanTubQA8D9z`;
+  
   fetch(api_request)
     .then(function (response) {
       return response.json();
     })
 
     .then(function (response) {
+      var randomNumber = Math.floor(Math.random() * response.data.length);
       $(".main").html(
-        `<img src="${response.data[`${randomNumber}`].images.original.url}>`
+        `<img src="${response.data[randomNumber].images.original.url}"/>`
       );
+      $(".modal").css("display","block");
     });
+  $("body").click(function(){
+    $(".modal").css("display","none");
+  });
 });
